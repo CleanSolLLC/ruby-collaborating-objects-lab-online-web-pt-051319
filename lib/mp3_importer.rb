4e852096f1b739973df73b16f628c308e67ba891
Mark May <mamark90@gmail.com>
2019-06-18 13:40:46 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class MP3Importer
+
+    def initialize(filename)
+        binding.pry
+
+    end
+
+
+    def files
+
+    end
+
+
+    def import
+
+
+    end
+
+
+  end
+
+
+
+
+
+end
