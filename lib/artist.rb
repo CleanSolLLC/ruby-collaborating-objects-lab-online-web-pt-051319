61213b0a90c7188751e48e4e6bb5af0ad0fc20f9
Mark May <mamark90@gmail.com>
2019-06-21 17:42:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,13 +26,14 @@ class Artist
     end
 
     def self.find_or_create_by_name(name)
-      self.create_by_name(name) || self.find_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
     end
 
     def self.create_by_name(name)
       artist = self.new(name)
       binding.pry
       artist.save
+      artist
     end
 
     def self.find_by_name(name)
