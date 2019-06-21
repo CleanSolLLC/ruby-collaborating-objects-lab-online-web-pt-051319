29c57c29a38654b47de2abbde2f0cd856a488fe2
Mark May <mamark90@gmail.com>
2019-06-21 16:51:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,14 +26,17 @@ class Artist
     end
 
     def self.find_or_create_by_name(name)
-      #@name = name
-      self.all.collect do |artist_name|
-        artist_name.name == name ? name : name = Artist.new(name)
-
-      end
-      name
+      self.find_by_name(name) || self.create_by_name(name)
     end
 
+    def self.create_by_name(name)
+      artist = Artist.new(name)
+      artist.save
+    end
+
+    def self.find_by_name
+      self.all.collect do |artist_name| artist_name.name == name}
+        
     def print_songs
       @songs.each {|song| puts song.name}
     end
