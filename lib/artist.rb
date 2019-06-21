7f8ad30e0e1989442c568c1e25c718776c07e8b3
Mark May <mamark90@gmail.com>
2019-06-21 16:58:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,16 +26,16 @@ class Artist
     end
 
     def self.find_or_create_by_name(name)
-      self.find_by_name(name) || self.create_by_name(name)
+      self.create_by_name(name) || self.find_by_name(name)
     end
 
     def self.create_by_name(name)
-      artist = Artist.new(name)
+      artist = self.new(name)
       artist.save
     end
 
     def self.find_by_name(name)
-      self.all.collect {|artist_name| artist_name.name == name}
+      self.all.detect {|artist_name| artist_name.name == name}
     end
 
     def print_songs
