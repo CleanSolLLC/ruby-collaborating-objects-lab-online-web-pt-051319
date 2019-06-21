bef593ef0dce59c72fcb714fcb69ea16cfa76aed
Mark May <mamark90@gmail.com>
2019-06-21 17:15:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -35,7 +35,7 @@ class Artist
     end
 
     def self.find_by_name(name)
-      self.all.detect {|artist_name| artist_name.name == name}
+      self.all.select {|artist_name| artist_name.name == name}
     end
 
     def print_songs
