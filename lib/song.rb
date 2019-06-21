67a2cb9668fa98646964db8fdaadb9fbbab3a00a
Mark May <mamark90@gmail.com>
2019-06-21 14:48:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,12 @@ class Song
       artist = files.split("-")[0].strip
       name = files.split("-")[1].strip
       song = self.new(name)
+      song.artist_name = artist
       song
     end
 
     def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
     end
 
