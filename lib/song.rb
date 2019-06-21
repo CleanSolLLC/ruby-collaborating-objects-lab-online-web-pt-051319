7c81a6a2b749a1f2029beb207b2ecd88f6a612fa
Mark May <mamark90@gmail.com>
2019-06-21 14:36:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
       artist = files.split("-")[0].strip
       name = files.split("-")[1].strip
       song = self.new(name)
-      song
       song.artist_name = artist
+      song
     end
 
     def artist_name=(name)
