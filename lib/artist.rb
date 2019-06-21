4d751d5170e18b877cde8eb4f9f59d1e97482674
Mark May <mamark90@gmail.com>
2019-06-21 16:21:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,6 +29,7 @@ class Artist
       #@name = name
       self.all.collect do |artist_name|
         artist_name.name == name ? name : name = Artist.new(name)
+        binding.pry
       end
       #name
     end
